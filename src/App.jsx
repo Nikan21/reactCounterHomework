fc8c10dd1a04618d176fb2abcf4e0e4a2a0b88d1
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-10-05 19:40:18 +0300

    Change function component on class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Counter from './Components/Counter/index'
-import './App.css';
+import Counter from "./Components/Counter/index";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: 3
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Counter step = {this.state.step}/>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
